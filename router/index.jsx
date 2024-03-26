53f20ce12b6d0b3d57891cbeea32ddda82264eb0
PrinceSP <princepasombaran@gmail.com>
2024-03-26 22:29:52 +0700

    feat: create routing between screen

---
+++ router/index.jsx
@@ -0,0 +1,27 @@
+import React, { useState,useEffect,useCallback } from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {Home} from "../pages"
+
+const Stack = createNativeStackNavigator();
+
+const Router = () => {
+
+  return (
+    <Stack.Navigator
+      screenOptions={
+        {
+          headerShown: false,
+          contentStyle:{
+            backgroundColor: '#FFFFFF'
+          }
+        }
+      }
+    >
+      <Stack.Screen name='Home' component={Home} options = {{headerShown:false}}></Stack.Screen>
+
+        {/* You can add you screen here */}
+    </Stack.Navigator>
+  );
+}
+
+export default Router
