a5f2076fcee356d2ba3e304a9eddcfefb6880e5a
PrinceSP <princepasombaran@gmail.com>
2024-03-27 23:01:09 +0700

    feat: ask for location permission - use expo-location package - sync the Location function with native permission with useEffect

--- App.js
+++ App.js
@@ -1,9 +1,23 @@
 import 'react-native-gesture-handler';
+import React,{useEffect,useState} from 'react'
 import { StatusBar } from 'react-native';
 import { NavigationContainer,DarkTheme } from '@react-navigation/native';
 import Router from './router'
+import * as Location from "expo-location"
 
 export default function App() {
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+    })();
+  }, []);
   return (
     <NavigationContainer theme={DarkTheme}>
       <StatusBar barStyle="light-content" backgroundColor="#121415"/>
