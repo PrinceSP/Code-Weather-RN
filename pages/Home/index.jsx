67e873f30c377142592dd87904d962b31f2f9490
PrinceSP <princepasombaran@gmail.com>
2024-03-27 23:08:32 +0700

    feat: finishing home screen components - hourly temperature flatlist component with horizontal scrolling - daily temperature component - current weather data component - integrate with One Call API froml OpenWeather API

--- pages/Home/index.jsx
+++ pages/Home/index.jsx
@@ -1,13 +1,15 @@
-import React,{useState} from "react"
+import React,{useState,memo} from "react"
 import {View,Text,StyleSheet,Image,SafeAreaView,TouchableOpacity,Dimensions,ScrollView,FlatList} from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { FontAwesome6,AntDesign } from '@expo/vector-icons';
+import { FontAwesome6,AntDesign,Octicons } from '@expo/vector-icons';
 import {LineChart} from "react-native-chart-kit";
+import {useGetData} from '../../custom-hooks'
+import {API_URL, API_KEY} from "@env"
 
 const {width,fontScale} = Dimensions.get('window')
 
 const styles = StyleSheet.create({
-  container:{flex:1, backgroundColor : "#121415"},
+  container:{height:"100%", backgroundColor : "#121415"},
   header: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -28,8 +30,23 @@ const chartConfig = {
   withDots:false
 };
 
+const HourlyWeather = memo(({ item })=>{
+  const unixTime = item.dt;
+  const exactDate = new Date(unixTime * 1000);
+  const time = exactDate.getHours().toString().padStart(2, '0') + ":" + exactDate.getMinutes().toString().padStart(2, '0');
+
+  return (
+    <View key={item.dt} style={{ alignItems: 'center', justifyContent: 'space-between', paddingVertical: 2, marginVertical: 20, marginRight: 10 }}>
+      <Text style={{ color: "#505050" }}>{time}</Text>
+      <Image source={{ uri: `https://openweathermap.org/img/wn/${item.weather[0].icon}.png` }} style={{ width: 35, height: 35 }} />
+      <Text style={{ color: "#fff" }}>{item.temp.toFixed(0)}˚F</Text>
+    </View>
+  );
+});
+
 const Home = ({navigation})=>{
   const insets = useSafeAreaInsets()
+  const currentDatas = useGetData(`${process.env.API_URL}?lat=33.44&lon=-94.04&appid=${process.env.API_KEY}&units=metric`)
 
   const data = {
     labels: ["January", "February", "March", "April", "May", "June"],
@@ -42,6 +59,8 @@ const Home = ({navigation})=>{
     ],
   };
 
+  const compassSector = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW", "N"];
+
   return(
     <SafeAreaView style={[styles.container,{paddingTop: insets.top}]}>
       <View style={styles.header}>
@@ -51,17 +70,17 @@ const Home = ({navigation})=>{
         </TouchableOpacity>
         <FontAwesome6 name="sliders" size={24} color="#fff" onPress={()=>navigation.navigate("Settings")}/>
       </View>
-      <ScrollView style={{paddingHorizontal:7,marginTop:37}}>
+      <ScrollView style={{paddingHorizontal:7,marginTop:37,paddingBottom:100}}>
         <View style={{alignSelf:'center',alignItems:'center'}}>
           <View style={{flexDirection:'row'}}>
-            <Image source={{uri:`https://openweathermap.org/img/wn/09d.png`}} style={{width:40,height:40}}/>
+            <Image source={{uri:`https://openweathermap.org/img/wn/${currentDatas?.current?.weather[0].icon}.png`}} style={{width:40,height:40}}/>
             <View>
-              <Text style={{color:"#fff"}}>Moderate rain</Text>
-              <Text style={{color:"#505050"}}>Light breeze</Text>
+              <Text style={{color:"#fff"}}>{currentDatas?.current?.weather[0]?.main}</Text>
+              <Text style={{color:"#505050"}}>{currentDatas?.current?.weather[0]?.description}</Text>
             </View>
           </View>
-          <Text style={{fontSize:80/fontScale,color:"#fff"}}>12˚C</Text>
-          <Text style={{color:"#505050"}}>Feels like 12˚C</Text>
+          <Text style={{fontSize:80/fontScale,color:"#fff"}}>{currentDatas?.current?.temp.toFixed(0)}˚C</Text>
+          <Text style={{color:"#505050"}}>Feels like {currentDatas?.current?.feels_like.toFixed(0)}˚C</Text>
         </View>
 
         <View style={{width,alignItems:'center',marginTop:50}}>
@@ -81,13 +100,37 @@ const Home = ({navigation})=>{
         </View>
 
         <View style={{flexDirection:'row',flexWrap:"wrap",alignItems:'center',justifyContent:'space-between',backgroundColor:"rgba(60,60,60,0.5)",padding:10,borderRadius:10,marginTop:40}}>
-          <Text style={{marginBottom:20,color:"#fff",fontSize:15/fontScale,}}>Wind: 11.3m/s WSW</Text>
-          <Text style={{marginBottom:20,color:"#fff",fontSize:15/fontScale,}}>Humidity: 69%</Text>
-          <Text style={{marginBottom:20,color:"#fff",fontSize:15/fontScale,}}>UV index: 0.0</Text>
-          <Text style={{marginBottom:20,color:"#fff",fontSize:15/fontScale,}}>Pressure: 994hPa</Text>
-          <Text style={{marginBottom:20,color:"#fff",fontSize:15/fontScale,}}>Visibility: 10.0km</Text>
-          <Text style={{marginBottom:20,color:"#fff",fontSize:15/fontScale,}}>Dew point: 1˚c</Text>
+          <Text style={{marginBottom:20,color:"#fff",fontSize:15/fontScale,}}>
+            Wind: {currentDatas?.current?.wind_speed}m/s {compassSector[(currentDatas?.current?.wind_deg / 22.5).toFixed(0)]}
+            <FontAwesome6 name="location-arrow" size={18} color="#aaa" style={{transform: [{rotate: `${currentDatas?.current?.wind_deg}deg`}]}}/>
+          </Text>
+          <Text style={{marginBottom:20,color:"#fff",fontSize:15/fontScale,}}>Humidity: {currentDatas?.current?.humidity}%</Text>
+          <Text style={{marginBottom:20,color:"#fff",fontSize:15/fontScale,}}>UV index: {currentDatas?.current?.uvi}</Text>
+          <Text style={{marginBottom:20,color:"#fff",fontSize:15/fontScale,}}>Pressure: {currentDatas?.current?.pressure}hPa</Text>
+          <Text style={{marginBottom:20,color:"#fff",fontSize:15/fontScale,}}>Visibility: {currentDatas?.current?.visibility}km</Text>
+          <Text style={{marginBottom:20,color:"#fff",fontSize:15/fontScale,}}>Dew point: {currentDatas?.current?.dew_point}˚C</Text>
         </View>
+
+        <FlatList
+          data={currentDatas?.hourly}
+          renderItem={({item})=><HourlyWeather item={item}/>}
+          keyExtractor={(item) => item.dt.toString()}
+          horizontal
+          maxToRenderPerBatch={10}
+        />
+
+        {
+          currentDatas?.daily.map((item)=>(
+            <TouchableOpacity style={{flexDirection:'row',alignItems:'center',justifyContent:'space-between',borderBottomStyle:"solid",borderBottomWidth:1,borderBottomColor:"#fff",paddingVertical:10}}>
+              <Text style={{color:"#fff",fontSize:16/fontScale}}>Wed Mar 27</Text>
+              <View style={{flexDirection:'row',alignItems:'center'}}>
+                <Text style={{color:"#fff",fontSize:16/fontScale}}>{item.temp.max.toFixed(0)} / {item.temp.min.toFixed(0)} ˚F</Text>
+                <Image source={{ uri: `https://openweathermap.org/img/wn/${item.weather[0].icon}.png` }} style={{ width: 38, height: 38, marginLeft:5,marginRight:10 }} />
+                <Octicons name="chevron-right" size={24} color="#888"/>
+              </View>
+            </TouchableOpacity>
+          ))
+        }
 
       </ScrollView>
     </SafeAreaView>
