c8ec8ae93bc196d32a12d0564ecc18901e7de4b3
PrinceSP <princepasombaran@gmail.com>
2024-03-27 15:23:29 +0700

    ref: refactoring custom units button and function

--- pages/Units/index.jsx
+++ pages/Units/index.jsx
@@ -1,7 +1,7 @@
 import React,{useState} from 'react'
 import {View,StyleSheet,Text,Image,TouchableOpacity,Dimensions,SafeAreaView} from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import {Header} from '../../components'
+import {Header,CustomUnits} from '../../components'
 
 const {width,fontScale} = Dimensions.get('window')
 
@@ -12,64 +12,47 @@ const styles = StyleSheet.create({
 const Units = ({navigation}) => {
   const insets = useSafeAreaInsets()
   const [temperature,setTemperature] = useState([
-    celcius:{
+    {
       isActive:true,
       type:"metrics",
       name:"˚C"
     },
-    fahrenheit:{
+    {
       isActive:false,
       type:"imperial",
       name:"˚F"
     }
   ])
-  const [windSpeed,setWindSpeed] = useState({
-    ms:{
-      isActive:true
-      type:"m/s"
+
+  const [windSpeed,setWindSpeed] = useState([
+    {
+      isActive:true,
+      type:"m/s",
+      name:"m/s"
     },
-    kmh:{
-      isActive:false
-      type:"km/h"
+    {
+      isActive:false,
+      type:"km/h",
+      name:"km/h"
     },
-    mph:{
-      isActive:false
-      type:"mph"
+    {
+      isActive:false,
+      type:"mph",
+      name:"mph"
     }
-  })
-  const handlePress = (index) => {
-    setTemperature(prevConditions =>
-      prevConditions.map((item, idx) => ({
-        ...item,
-        isActive: idx === index,
-      }))
-    );
-  };
+  ])
+
   return (
     <SafeAreaView style={[styles.container,{paddingTop: insets.top}]}>
       <Header navigation={navigation} title="Units"/>
       <View style={{paddingHorizontal:6,paddingVertical:10,borderBottomWidth:1,borderBottomColor:"#ccc",borderBottomStyle:"solid",alignItems:'center',justifyContent:'space-between',flexDirection:'row'}}>
         <Text style={{color:"#ccc",fontSize:16/fontScale}}>Temperature</Text>
-          <View style={{width:'50%',flexDirection:'row',padding:4,borderRadius:6,backgroundColor:"rgba(100, 100, 100, 0.16)"}}>
-            {temperature.maps((item,index)=><TouchableOpacity style={{width:'50%',paddingVertical:2,alignItems:'center',justifyContent:'center',backgroundColor:item.isActive === true ? "#77777755" : "rgba(0,0,0,0)",borderRadius:3}} onPress={()=>setTemperature({...temperature,isActiveeeeereere})}>
-              <Text style={{color:"#ccc",fontSize:14/fontScale}}>{item.name}</Text>
-            </TouchableOpacity>)}
-          </View>
+        <CustomUnits width="50%" data={temperature} setData={setTemperature}/>
       </View>
 
       <View style={{marginTop:12,paddingHorizontal:6,paddingVertical:10,borderBottomWidth:1,borderBottomColor:"#ccc",borderBottomStyle:"solid",alignItems:'center',justifyContent:'space-between',flexDirection:'row'}}>
         <Text style={{color:"#ccc",fontSize:16/fontScale}}>Wind speed</Text>
-          <View style={{width:'50%',flexDirection:'row',padding:4,borderRadius:6,backgroundColor:"rgba(100, 100, 100, 0.16)"}}>
-            <TouchableOpacity style={{width:'33.33%',paddingVertical:2,alignItems:'center',justifyContent:'center',backgroundColor:"#77777755",borderRadius:3}}>
-              <Text style={{color:"#ccc",fontSize:14/fontScale}}>m/s</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={{width:'33.33%',paddingVertical:2,alignItems:'center',justifyContent:'center',backgroundColor:"#77777755",borderRadius:3}}>
-              <Text style={{color:"#ccc",fontSize:14/fontScale}}>km/h</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={{width:'33.33%',paddingVertical:2,alignItems:'center',justifyContent:'center',backgroundColor:"#77777755",borderRadius:3}}>
-              <Text style={{color:"#ccc",fontSize:14/fontScale}}>mph</Text>
-            </TouchableOpacity>
-          </View>
+        <CustomUnits width="33.33%" data={windSpeed} setData={setWindSpeed}/>
       </View>
 
       <View style={{marginTop:12,paddingHorizontal:6,paddingVertical:10,borderBottomWidth:1,borderBottomColor:"#ccc",borderBottomStyle:"solid",alignItems:'center',justifyContent:'space-between',flexDirection:'row'}}>
