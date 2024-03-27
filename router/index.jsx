5483505cff41da50f83e21b4a052f0c9aa182cf0
PrinceSP <princepasombaran@gmail.com>
2024-03-27 14:28:39 +0700

    feat: add Unit screen route

--- router/index.jsx
+++ router/index.jsx
@@ -1,8 +1,28 @@
 import React, { useState,useEffect,useCallback } from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {Home,Settings,DifferentWeather} from "../pages"
+import { Easing } from 'react-native';
+import { createNativeStackNavigator,TransitionPreset,CardStyleInterpolators } from '@react-navigation/native-stack';
+import {Home,Settings,DifferentWeather,Units} from "../pages"
 
 const Stack = createNativeStackNavigator();
+
+const config = {
+  animation: 'spring',
+  config: {
+    stiffness: 1000,
+    damping: 500,
+    mass: 3,
+    overshootClamping: true,
+    restDisplacementThreshold: 0.01,
+    restSpeedThreshold: 0.01,
+  },
+};
+const closeconfig = {
+  animation: 'timing',
+  config: {
+    duration:500,
+    easing:Easing.linear
+  },
+};
 
 const Router = () => {
 
@@ -10,16 +30,23 @@ const Router = () => {
     <Stack.Navigator
       screenOptions={
         {
+          gestureEnabled:true,
+          gestureDirection:"horizontal",
+          transitionSpec:{
+            open:config,
+            close:config
+          },
           headerShown: false,
-          contentStyle:{
-            backgroundColor: '#FFFFFF'
-          }
+          cardStyle: {
+            backgroundColor: '#fff',
+          },
         }
       }
     >
-      <Stack.Screen name='Home' component={Home} options = {{headerShown:false}}></Stack.Screen>
-      <Stack.Screen name='Settings' component={Settings} options = {{headerShown:false}}></Stack.Screen>
-      <Stack.Screen name='DifferentWeather' component={DifferentWeather} options = {{headerShown:false}}></Stack.Screen>
+      <Stack.Screen name='Home' component={Home}></Stack.Screen>
+      <Stack.Screen name='Settings' component={Settings}></Stack.Screen>
+      <Stack.Screen name='DifferentWeather' component={DifferentWeather}></Stack.Screen>
+      <Stack.Screen name='Units' component={Units}></Stack.Screen>
 
         {/* You can add you screen here */}
     </Stack.Navigator>
