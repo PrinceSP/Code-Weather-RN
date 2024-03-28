74432ea94d1fe7a6fd35f5edf0064b2c9e0a5765
PrinceSP <princepasombaran@gmail.com>
2024-03-28 14:29:11 +0700

    feat: add redux dispatcher function

--- components/molecules/CustomUnits/index.jsx
+++ components/molecules/CustomUnits/index.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import {View,TouchableOpacity,Text,Dimensions} from 'react-native'
+import React from 'react';
+import { View, TouchableOpacity, Text, Dimensions } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { changeUnit } from '../../../store/unitsMeasurements'; // Import the action creator
 
-const {width,fontScale} = Dimensions.get('window')
+const { width, fontScale } = Dimensions.get('window');
 
-const CustomUnits = ({width,data,setData}) => {
+const CustomUnits = ({ width, data, setData }) => {
+  const dispatch = useDispatch();
+
   const handlePress = (index) => {
+    const selectedUnit = data[index].type;
+    const selectedType = data[index].label.toLowerCase(); // Get the type from the selected item
+    dispatch(changeUnit({ type: selectedType, label: selectedUnit })); // Dispatch the action
     setData(prevDatas =>
       Array.isArray(prevDatas) ?
       prevDatas.map((item, idx) => ({
@@ -23,7 +30,7 @@ const CustomUnits = ({width,data,setData}) => {
 
   return (
     <View style={{ width: '50%', flexDirection: 'row', padding: 4, borderRadius: 6, backgroundColor: "rgba(100, 100, 100, 0.16)" }}>
-      {data.map((item,index) => (
+      {data.map((item, index) => (
         <TouchableOpacity
           key={item.name}
           style={{ width, paddingVertical: 2, alignItems: 'center', justifyContent: 'center', backgroundColor: item.isActive ? "#77777755" : "rgba(0,0,0,0)", borderRadius: 3 }}
@@ -33,7 +40,7 @@ const CustomUnits = ({width,data,setData}) => {
         </TouchableOpacity>
       ))}
     </View>
-  )
-}
+  );
+};
 
-export default CustomUnits
+export default CustomUnits;
