f36449728895ae5439a88acafec1c850d01c8f56
PrinceSP <princepasombaran@gmail.com>
2024-03-28 15:32:26 +0700

    feat: add static precipitation data configuration

---
+++ configs/precipitationData/index.js
@@ -0,0 +1,10 @@
+export const data = {
+  labels: ["January", "February", "March", "April", "May", "June"],
+  datasets: [
+    {
+      data: [0.5,1],
+      withDots: false,
+      strokeWidth: 1 // optional
+    }
+  ],
+};
