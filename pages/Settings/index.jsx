4713dc8069a62ffbe301d6ebcfedf92e0a088db7
PrinceSP <princepasombaran@gmail.com>
2024-03-28 16:17:48 +0700

    feat: use setting menu comopnent

--- pages/Settings/index.jsx
+++ pages/Settings/index.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import {View,StyleSheet,Text,Image,TouchableOpacity,Dimensions,SafeAreaView} from 'react-native'
-import {Header} from '../../components'
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { Octicons } from '@expo/vector-icons';
 import openweatherFullLogo from "../../assets"
+import {Header,SettingMenu} from '../../components'
 
 const {width,fontScale} = Dimensions.get('window')
 
@@ -24,14 +23,8 @@ const Settings = ({navigation}) => {
     <SafeAreaView style={[styles.container,{paddingTop: insets.top}]}>
       <Header navigation={navigation} title="Settings"/>
       <View style={styles.content}>
-        <TouchableOpacity style={{paddingHorizontal:6,paddingVertical:12,borderBottomWidth:1,borderBottomColor:"#ccc",borderBottomStyle:"solid",alignItems:'center',justifyContent:'space-between',flexDirection:'row'}} onPress={()=>navigation.navigate("DifferentWeather")}>
-          <Text style={{color:"#ccc",fontSize:16/fontScale}}>Different weather?</Text>
-          <Octicons name="chevron-right" size={28} color="#ccc" />
-        </TouchableOpacity>
-        <TouchableOpacity style={{paddingHorizontal:6,paddingVertical:10,borderBottomWidth:1,borderBottomColor:"#ccc",borderBottomStyle:"solid",alignItems:'center',justifyContent:'space-between',flexDirection:'row'}} onPress={()=>navigation.navigate("Units")}>
-          <Text style={{color:"#ccc",fontSize:16/fontScale}}>Customize units</Text>
-          <Octicons name="chevron-right" size={28} color="#ccc" />
-        </TouchableOpacity>
+        <SettingMenu navigation={navigation} screen="DifferentWeather" title="Different weather?"/>
+        <SettingMenu navigation={navigation} screen="Units" title="Units"/>
         <View style={{width:width*0.85,marginVertical:10,marginLeft:6,paddingVertical:10,alignItems:'center',justifyContent:'space-between',flexDirection:'row'}}>
           <Text style={{color:"#ccc",fontSize:16/fontScale}}>Data</Text>
           <Text style={{color:"#707070",fontSize:16/fontScale}}>One Call API™</Text>
