34e1af4738548c556729a49c5658df0b90996470
PrinceSP <princepasombaran@gmail.com>
2024-03-28 15:30:57 +0700

    feat: create chart configuration for LineChart component

---
+++ configs/chartConfig/index.js
@@ -0,0 +1,10 @@
+const chartConfig = {
+  backgroundGradientFrom: "#1E2923",
+  backgroundGradientFromOpacity: 0,
+  backgroundGradientTo: "#08130D",
+  backgroundGradientToOpacity: 0.5,
+  color: (opacity = 1) => `rgba(170, 170, 170, ${opacity})`,
+  strokeWidth: 1, // optional, default 3
+  withDots:false
+};
+export default chartConfig
