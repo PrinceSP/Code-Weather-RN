787a259926b83aee4bfafd962615611f4c439866
PrinceSP <princepasombaran@gmail.com>
2024-04-01 13:26:20 +0700

    feat: mocked version of AsyncStorage when running test with Jest

---
+++ setup.js
@@ -0,0 +1,5 @@
+// setup.js
+
+import MockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+
+jest.mock('@react-native-async-storage/async-storage', () => MockAsyncStorage);
