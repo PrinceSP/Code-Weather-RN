79305e3b6ebc3d728acbb7b91870d253b32b926c
PrinceSP <princepasombaran@gmail.com>
2024-04-01 13:33:01 +0700

    chore: export search location page

--- pages/index.js
+++ pages/index.js
@@ -2,5 +2,6 @@ import Home from './Home'
 import Settings from './Settings'
 import DifferentWeather from './DifferentWeather'
 import Units from './Units'
+import SearchLocation from './SearchLocation'
 
-export {Home,Settings,DifferentWeather,Units}
+export {Home,Settings,DifferentWeather,Units,SearchLocation}
