367952b5c0e5f729ce4f5a88f90af4e1b8a83d43
PrinceSP <princepasombaran@gmail.com>
2024-04-01 15:27:49 +0700

    test: create unit test for main content wrapper

---
+++ __test__/App.test.js
@@ -0,0 +1,13 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from '../App';
+
+describe('<App />', () => {
+  const tree = renderer.create(<App />).toJSON();
+
+  it('renders correctly', () => {
+    expect(tree).toMatchSnapshot();
+  });
+
+});
