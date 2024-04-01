dfcf08460d0d47d9b2742adcb867ba9873d3131e
PrinceSP <princepasombaran@gmail.com>
2024-04-01 14:14:14 +0700

    ref: move location request permission to findLocation component

--- App.js
+++ App.js
@@ -3,23 +3,10 @@ import React,{useEffect,useState} from 'react'
 import { StatusBar } from 'react-native';
 import { NavigationContainer,DarkTheme } from '@react-navigation/native';
 import Router from './router'
-import * as Location from "expo-location"
 import {Provider} from 'react-redux'
 import {store} from './store'
 
 export default function App() {
-  const [errorMsg, setErrorMsg] = useState(null);
-
-  useEffect(() => {
-    (async () => {
-
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
-    })();
-  }, []);
   return (
     <Provider store={store}>
       <NavigationContainer theme={DarkTheme}>
